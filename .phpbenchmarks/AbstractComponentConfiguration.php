0f4395dc3796fb1e5061fe5c36995bd94026d99a
Steevan <steevan.barboyon@gmail.com>
2019-05-18 19:05:57 +0200

    Fix source code urls

--- .phpbenchmarks/AbstractComponentConfiguration.php
+++ .phpbenchmarks/AbstractComponentConfiguration.php
@@ -79,15 +79,15 @@ abstract class AbstractComponentConfiguration
     public static function getSourceCodeUrls(): array
     {
         return [
-            'jsonSerialization' => 'https://github.com/phpbenchmarks/symfony-serializer',
-            'integerSerialization' => 'https://github.com/phpbenchmarks/symfony-serializer',
-            'floatSerialization' => 'https://github.com/phpbenchmarks/symfony-serializer',
-            'stringSerialization' => 'https://github.com/phpbenchmarks/symfony-serializer',
-            'booleanSerialization' => 'https://github.com/phpbenchmarks/symfony-serializer',
-            'nullSerialization' => 'https://github.com/phpbenchmarks/symfony-serializer',
-            'arraySerialization' => 'https://github.com/phpbenchmarks/symfony-serializer',
-            'objectSerialization' => 'https://github.com/phpbenchmarks/symfony-serializer',
-            'customSerializers' => 'https://github.com/phpbenchmarks/symfony-serializer'
+            'jsonSerialization' => 'https://github.com/phpbenchmarks/symfony-serializer-common/blob/4.7.0/src/benchmark.php#L21',
+            'integerSerialization' => 'https://github.com/phpbenchmarks/symfony-serializer-common/blob/4.7.0/src/benchmark.php#L21',
+            'floatSerialization' => 'https://github.com/phpbenchmarks/symfony-serializer-common/blob/4.7.0/src/benchmark.php#L21',
+            'stringSerialization' => 'https://github.com/phpbenchmarks/symfony-serializer-common/blob/4.7.0/src/benchmark.php#L21',
+            'booleanSerialization' => 'https://github.com/phpbenchmarks/symfony-serializer-common/blob/4.7.0/src/benchmark.php#L21',
+            'nullSerialization' => 'https://github.com/phpbenchmarks/symfony-serializer-common/blob/4.7.0/src/benchmark.php#L21',
+            'arraySerialization' => 'https://github.com/phpbenchmarks/symfony-serializer-common/blob/4.7.0/src/benchmark.php#L21',
+            'objectSerialization' => 'https://github.com/phpbenchmarks/symfony-serializer-common/blob/4.7.0/src/Normalizer/ObjectToSerializeNormalizer.php#L16',
+            'customSerializers' => 'https://github.com/phpbenchmarks/symfony-serializer-common/tree/4.7.0/src/Normalizer'
         ];
     }
 }
