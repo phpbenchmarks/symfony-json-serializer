93bce6fdb5373180cbdee2c781e315c1cb90f791
Steevan BARBOYON <steevan.barboyon@gmail.com>
2020-03-26 16:36:29 +0100

    Small overload (json serializer) benchmark for Symfony JSON serializer 5.0.0

---
+++ public/index.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+require(__DIR__ . '/../vendor/autoload.php');
+
+use PhpBenchmarks\BenchmarkJsonSerializationSmallOverload\BenchmarkService;
+use Symfony\Component\Serializer\{
+    Encoder\JsonEncoder,
+    Serializer
+};
+use PhpBenchmarks\SymfonyJsonSerializer\Normalizer\ObjectToSerializeNormalizer;
+
+$normalizers = [];
+for ($i = 1; $i <= 99; $i++) {
+    $class = "PhpBenchmarks\\SymfonyJsonSerializer\\Normalizer\\OverloadNormalizer$i";
+    $normalizers[] = new $class();
+}
+$normalizers[] = new ObjectToSerializeNormalizer();
+
+$serializer = new Serializer($normalizers, [new JsonEncoder()]);
+$benchmarkService = new BenchmarkService();
+
+if ($benchmarkService->isWriteToResponseBody()) {
+    echo $serializer->serialize($benchmarkService->getDataToSerialize(), 'json');
+} else {
+    $serializer->serialize($benchmarkService->getDataToSerialize(), 'json');
+}
