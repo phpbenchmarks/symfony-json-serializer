41bff04cf23fdbbe407b4e9d31b422952de7d4a9
Steevan BARBOYON <steevan.barboyon@gmail.com>
2020-03-28 12:21:20 +0100

    Big overload (json serializer) benchmark for Symfony JSON serializer 5.0.0

---
+++ src/Normalizer/OverloadNormalizer212.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpBenchmarks\SymfonyJsonSerializer\Normalizer;
+
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class OverloadNormalizer212 implements NormalizerInterface
+{
+    public function supportsNormalization($data, $format = null, array $context = [])
+    {
+        return $data instanceof UnknownObject;
+    }
+
+    public function normalize($object, $format = null, array $context = [])
+    {
+        throw new \Exception(__CLASS__ . '::' . __FUNCTION__ . ' should not be called.');
+    }
+}
