ede4cb5dee2b1a181824da366aed9a49a840fd54
Steevan BARBOYON <steevan.barboyon@gmail.com>
2020-03-30 14:09:34 +0200

    Small overload (json serializer) benchmark for Symfony JSON serializer 5.0.6

--- .phpbenchmarks/php/7.4/preload.php
+++ .phpbenchmarks/php/7.4/preload.php
@@ -1,5 +1,181 @@
 <?php
+/**
+ * Preloader Script 2.x
+ *
+ * This file is generated automatically by the Preloader package.
+ *
+ * The following script uses `opcache_compile_file($file)` syntax to preload each file in this list into Opcache.
+ * To full enable preload, add this file to your `php.ini` in `opcache.preload` key to preload
+ * this list of files PHP at startup. This file also includes some information about Opcache.
+ *
+ *
+ * Add (or update) this line in `php.ini`:
+ *
+ *     opcache.preload=/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/.phpbenchmarks/php/7.4/preload.php
+ *
+ *
+ * --- Config ---
+ * Generated at: 2020-03-30 14:09:33 Europe/Berlin
+ * Opcache
+ *     - Used Memory: 9.2 MB
+ *     - Free Memory: 118.8 MB
+ *     - Wasted Memory: 0.0 MB
+ *     - Cached files: 133
+ *     - Hit rate: 0.00%
+ *     - Misses: 134
+ * Preloader config
+ *     - Memory limit: 32 MB
+ *     - Overwrite: true
+ *     - Files excluded: 0
+ *     - Files appended: 0
+ *
+ *
+ * For more information:
+ * @see https://github.com/darkghosthunter/preloader
+ */
 
-declare(strict_types=1);
 
-require(__DIR__ . '/../../../public/index.php');
+
+$files = [
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/public/index.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/ObjectToSerializeNormalizer.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer1.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer10.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer11.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer12.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer13.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer14.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer15.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer16.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer17.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer18.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer19.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer2.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer20.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer21.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer22.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer23.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer24.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer25.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer26.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer27.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer28.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer29.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer3.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer30.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer31.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer32.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer33.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer34.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer35.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer36.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer37.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer38.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer39.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer4.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer40.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer41.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer42.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer43.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer44.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer45.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer46.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer47.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer48.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer49.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer5.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer50.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer51.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer52.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer53.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer54.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer55.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer56.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer57.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer58.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer59.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer6.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer60.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer61.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer62.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer63.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer64.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer65.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer66.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer67.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer68.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer69.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer7.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer70.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer71.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer72.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer73.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer74.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer75.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer76.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer77.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer78.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer79.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer8.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer80.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer81.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer82.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer83.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer84.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer85.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer86.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer87.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer88.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer89.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer9.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer90.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer91.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer92.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer93.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer94.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer95.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer96.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer97.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer98.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/src/Normalizer/OverloadNormalizer99.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/vendor/autoload.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/vendor/composer/ClassLoader.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/vendor/phpbenchmarks/benchmark-json-serialization-small-overload/src/BenchmarkService.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/vendor/phpbenchmarks/benchmark-json-serialization-small-overload/src/ObjectToSerialize/ObjectToSerialize.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/vendor/phpbenchmarks/benchmark-json-serialization-small-overload/src/ObjectToSerialize/ObjectToSerializeInterface.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/vendor/phpbenchmarks/benchmark-json-serialization-small-overload/src/ObjectToSerializeFactory/ObjectToSerializeFactory.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/vendor/phpbenchmarks/benchmark-json-serialization-small-overload/src/ObjectToSerializeFactory/ObjectToSerializeFactoryInterface.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/vendor/symfony/polyfill-ctype/bootstrap.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/vendor/symfony/serializer/Encoder/ChainDecoder.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/vendor/symfony/serializer/Encoder/ChainEncoder.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/vendor/symfony/serializer/Encoder/ContextAwareDecoderInterface.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/vendor/symfony/serializer/Encoder/ContextAwareEncoderInterface.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/vendor/symfony/serializer/Encoder/DecoderInterface.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/vendor/symfony/serializer/Encoder/EncoderInterface.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/vendor/symfony/serializer/Encoder/JsonDecode.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/vendor/symfony/serializer/Encoder/JsonEncode.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/vendor/symfony/serializer/Encoder/JsonEncoder.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/vendor/symfony/serializer/Normalizer/ContextAwareDenormalizerInterface.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/vendor/symfony/serializer/Normalizer/ContextAwareNormalizerInterface.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/vendor/symfony/serializer/Normalizer/DenormalizerInterface.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/vendor/symfony/serializer/Normalizer/NormalizerInterface.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/vendor/symfony/serializer/Serializer.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/vendor/symfony/serializer/SerializerInterface.php',
+    '/var/www/phpbenchmarks/project/generator/var/generator/5e81e174538c2/generated/.phpbenchmarks/php/7.4/preload.php'
+];
+
+foreach ($files as $file) {
+    try {
+        if (!(is_file($file) && is_readable($file))) {
+            throw new \Exception("{$file} does not exist or is unreadable.");
+        }
+        opcache_compile_file($file);
+    } catch (\Throwable $e) {
+        echo 'Preloader Script has stopped with an error:' . \PHP_EOL;
+        echo 'Message: ' . $e->getMessage() . \PHP_EOL;
+        echo 'File: ' . $file . \PHP_EOL;
+
+        throw $e;
+    }
+}
+
