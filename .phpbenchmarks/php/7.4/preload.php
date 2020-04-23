22bf7b0163d3e4b54be69801b0fe95e38ea8e78c
Steevan BARBOYON <steevan.barboyon@gmail.com>
2020-04-23 17:01:17 +0200

    Hello world (json serializer) benchmark for Symfony JSON serializer 5.0.6

--- .phpbenchmarks/php/7.4/preload.php
+++ .phpbenchmarks/php/7.4/preload.php
@@ -1,5 +1,33 @@
 <?php
 
-declare(strict_types=1);
+$sourceCodePath = __DIR__ . '/../../../';
+$files = [
+    "$sourceCodePath/public/index.php",
+    "$sourceCodePath/vendor/autoload.php",
+    "$sourceCodePath/vendor/composer/ClassLoader.php",
+    "$sourceCodePath/vendor/phpbenchmarks/benchmark-json-serialization-hello-world/src/BenchmarkService.php",
+    "$sourceCodePath/vendor/symfony/polyfill-ctype/bootstrap.php",
+    "$sourceCodePath/vendor/symfony/serializer/Encoder/ChainDecoder.php",
+    "$sourceCodePath/vendor/symfony/serializer/Encoder/ChainEncoder.php",
+    "$sourceCodePath/vendor/symfony/serializer/Encoder/ContextAwareDecoderInterface.php",
+    "$sourceCodePath/vendor/symfony/serializer/Encoder/ContextAwareEncoderInterface.php",
+    "$sourceCodePath/vendor/symfony/serializer/Encoder/DecoderInterface.php",
+    "$sourceCodePath/vendor/symfony/serializer/Encoder/EncoderInterface.php",
+    "$sourceCodePath/vendor/symfony/serializer/Encoder/JsonDecode.php",
+    "$sourceCodePath/vendor/symfony/serializer/Encoder/JsonEncode.php",
+    "$sourceCodePath/vendor/symfony/serializer/Encoder/JsonEncoder.php",
+    "$sourceCodePath/vendor/symfony/serializer/Normalizer/ContextAwareDenormalizerInterface.php",
+    "$sourceCodePath/vendor/symfony/serializer/Normalizer/ContextAwareNormalizerInterface.php",
+    "$sourceCodePath/vendor/symfony/serializer/Normalizer/DenormalizerInterface.php",
+    "$sourceCodePath/vendor/symfony/serializer/Normalizer/NormalizerInterface.php",
+    "$sourceCodePath/vendor/symfony/serializer/Serializer.php",
+    "$sourceCodePath/vendor/symfony/serializer/SerializerInterface.php",
+];
 
-require(__DIR__ . '/../../../public/index.php');
+foreach ($files as $file) {
+    if (is_readable($file) === false) {
+        throw new \Exception('File "' . $file . '" is not readable.');
+    }
+
+    opcache_compile_file($file);
+}
